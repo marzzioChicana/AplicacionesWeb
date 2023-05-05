487b6e0e65421bf134682fb93e2f985f1b85da5b
marzzioChicana <u202022228@upc.edu.pe>
2023-05-05 07:55:35 -0500

    final push login/register

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,11 @@
 <template>
-    <div class="w-full">
-        <pv-toolbar>
-            <template #start>
-                <img src="./assets/logo.png" alt="" width="100" height="30">&nbsp
-                <pv-button label="Servicios" severity="success" text />
-                <pv-button label="Nosotros" severity="success" text />
-                <pv-button label="Soporte" severity="success" text />
-            </template>
-
-            <template #end>
-
-                <pv-button label="Iniciar Sesion" severity="success" outlined/>&nbsp &nbsp
-                <pv-button label="Registrar" severity="success" severities />
-            </template>
-        </pv-toolbar>
-    </div>
-    <div>
-        <img src="./assets/fondo.jpg" alt="" width="1520" height="650">
+    <div id="app">
+        <router-view></router-view>
     </div>
 </template>
 
 <script>
-import Inicio from '@/components/inicio.component.vue'
-import inicioComponent from "@/components/inicio.component.vue";
-
 export default{
-    name:'App',
-    computed: {
-        inicioComponent() {
-            return inicioComponent
-        }
-    },
-    components:{Inicio}
+    name:'App'
 }
 </script>
