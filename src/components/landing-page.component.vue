f0af4e9f53df8220bea1c76188c07e5557633ab9
JosueCarrascoH <89035938+JosueCarrascoH@users.noreply.github.com>
2023-07-01 00:43:35 -0500

    añadiendo el video about the produtc

--- src/components/landing-page.component.vue
+++ src/components/landing-page.component.vue
@@ -132,6 +132,13 @@
                 </div>
             </div>
         </div>
+        <div class="row">
+            <div class="col">
+                <div class="video-container">
+                    <iframe width="100%" height="600" src="https://www.youtube.com/embed/tgubZAMEu-I" frameborder="0" allowfullscreen></iframe>
+                </div>
+            </div>
+        </div>
 
     </div>
 
